261982bee858d3f171761605e19f8f86bcf8adc4
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-04-05 17:46:43 -0700

    feat: add responses
    Respond to the user interactions in the slime command.

--- src/commands/slime.ts
+++ src/commands/slime.ts
@@ -1,11 +1,21 @@
+import { logHandler } from "../helpers/logHandler";
 import { CommandInt } from "../interfaces/CommandInt";
 
 export const slime: CommandInt = {
   name: "slime",
   description: "Gives you a slimy nickname~!",
   run: async (message) => {
-    const { guild, member } = message;
+    try {
+      const { channel, member } = message;
 
-    await member?.setNickname("testing");
+      await member?.setNickname("testing");
+      await channel.send("You've been slimed!");
+    } catch (error) {
+      logHandler.log("error", {
+        errorMessage: error.message,
+        errorStack: error.stack,
+      });
+      await message.channel.send("Oh no! My slime cannon broke!");
+    }
   },
 };
